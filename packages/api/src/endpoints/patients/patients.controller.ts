8311a3654e8c2feecdfbd84c979400f794ad126e
Thibault LR <thibault.lr@outlook.com>
2024-03-13 00:36:51 +0100

    feat(api): init swagger

--- packages/api/src/endpoints/patients/patients.controller.ts
+++ packages/api/src/endpoints/patients/patients.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { Patient } from '@prisma/client';
 import { PatientsRetrievalService } from './patients-retrieval.service';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('patients')
 export class PatientsController {
   public constructor(private readonly patientsRetrievalService: PatientsRetrievalService) {}
 
   @Get('/')
+  @ApiOkResponse()
   public async findAllPatients(): Promise<Patient[]> {
     const patients = await this.patientsRetrievalService.findAll();
 
--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
+import { version } from '../package.json';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder().setTitle('Embleema API').setVersion(version).build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
