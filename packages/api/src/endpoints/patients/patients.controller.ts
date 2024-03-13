2b504e3e9bdb473428724c2ecde2de714b2b4340
Thibault LR <thibault.lr@outlook.com>
2024-03-13 01:07:10 +0100

    chore: fix missing variables

--- packages/api/src/endpoints/patients/patients.controller.ts
+++ packages/api/src/endpoints/patients/patients.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiOkResponse } from '@nestjs/swagger';
+import { ApiOkResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { PatientsRetrievalService } from './patients-retrieval.service';
 import { Patient } from './dto/patient.dto';
 
@@ -8,6 +8,7 @@ export class PatientsController {
   public constructor(private readonly patientsRetrievalService: PatientsRetrievalService) {}
 
   @Get('/')
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   @ApiOkResponse({ type: Patient, isArray: true })
   public async findAllPatients(): Promise<Patient[]> {
     const patients = await this.patientsRetrievalService.findAll();
