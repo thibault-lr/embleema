eb4099ab514fcfca0a128b51daf2b541f613b7e7
Thibault LR <thibault.lr@outlook.com>
2024-03-13 01:13:18 +0100

    fix: ci

--- packages/api/src/repository/patient.repository.spec.ts
+++ packages/api/src/repository/patient.repository.spec.ts
@@ -28,6 +28,7 @@ describe('PatientRepository', () => {
 
   describe('#findAll', () => {
     it('returns the list of the patients', async () => {
+      await prismaClient.patient.count()
       await prismaClient.patient.create({ data: PATIENT_MOCK });
 
       const patients = await patientRepository.findAll();
